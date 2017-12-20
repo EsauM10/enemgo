956551a2b9ee441a36adb2afa5e9ace6990ff735
bnrosa <bernardoorosa@gmail.com>
2017-12-20 11:03:58 -0300

    Create user admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create kind: :admin, email: 'admin@teste.com', password: 123456
