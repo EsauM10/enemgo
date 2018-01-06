82747b16471d42fb03227612becfc4d9a982f8ff
Velder Soares <velder.vas@gmail.com>
2018-01-06 01:47:42 -0300

    make small changes on sidebar

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,9 +2,9 @@ module DashboardHelper
   def render_options_menu
     case current_user.kind
     when 'student'
-      render 'admin/dashboard/menu'
-    when 'admin'
       render 'student/dashboard/menu'
+    when 'admin'
+      render 'admin/dashboard/menu'
     end
   end
 end
